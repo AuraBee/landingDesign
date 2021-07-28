4efb72e1390fa5813ffa5c8c8ad5abb6402d5ac7
Be Sky <skyaze@outlook.com>
2021-07-28 18:42:49 +0100

    second commit

--- index.js
+++ index.js
@@ -13,3 +13,62 @@ open.addEventListener('click', () => {
 exit.addEventListener('click', () => {
     nav.classList.remove('open-nav');
 })
+
+
+
+const skill = [
+    {
+        name: 'HTML5',
+            thumbnail: '#',
+    },
+     {
+        name: 'CSS3',
+        thumbnail: '#',  
+    },
+     {
+        name: 'JavaScript',
+        thumbnail: '#',
+      
+    },
+    
+    { 
+        name: 'ReactJs',
+        thumbnail: '#',
+    
+    },
+    
+    {
+        name: 'mySQL',
+        thumbnail: '#',
+        //credit:
+    },
+    
+        
+    {
+        name: 'PHP',
+        thumbnail: '#',
+        //credit:
+    },
+
+    
+    
+]
+
+const skillBasket = document.getElementById("skill-basket")
+
+
+skill.forEach((image) => {
+    const card = document.createElement('div')
+    card.classList.add('card');
+    skillBasket.appendChild(card)
+
+    const picture = document.createElement('img')
+    picture.classList.add('picture');
+    picture.src = image.thumbnail
+    card.appendChild(picture)
+
+    const imageName = document.createElement('h2')
+    imageName.classList.add('image-name');
+    imageName.innerText = image.name
+    card.appendChild(imageName)
+})
